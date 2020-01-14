c51fafa72d8c3019fa1ff5ab754b809aa11190bb
Victor Rocha <vrsarmento@gmail.com>
2020-01-13 21:29:37 -0300

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>Aplicação 2020 - \o/ \o/</div>
+    )
+  }
+})
+
+export default App

9e3a60a0b9c330fa751b5d39cbfc99e4e4d55766
Victor Rocha <vrsarmento@gmail.com>
2020-01-16 20:31:49 -0300

    App initial markup

--- src/app.js
+++ src/app.js
@@ -2,12 +2,44 @@
 
 import React from 'react'
 
-const App = React.createClass({
-  render: function () {
-    return (
-      <div>Aplicação 2020 - \o/ \o/</div>
-    )
-  }
-})
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input type='search' placeholder='Digite o nome de usuário no GitHub' />
+    </div>
+
+    <div className='user-info'>
+      <img src='https://avatars2.githubusercontent.com/u/2380231?v=4' />
+      <h1>
+        <a href='https://github.com/vrsarmento'>Victor Rocha</a>
+      </h1>
+
+      <ul className='repos-info'>
+        <li>Repositórios: 122</li>
+        <li>Seguidores: 12</li>
+        <li>Seguindo: 30</li>
+      </ul>
+
+      <div className='actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
+      </div>
+
+      <div className='repos'>
+        <h2>Repositórios</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
 
 export default App

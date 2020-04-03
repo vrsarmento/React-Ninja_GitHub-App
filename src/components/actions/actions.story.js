467aa2b88ced5d8e1de08f6f115bb26895e01278
Victor Rocha <vrsarmento@gmail.com>
2020-04-02 22:06:27 -0300

    Storybook install, basic config and examples

---
+++ src/components/actions/actions.story.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+
+storiesOf('Action', module)
+  .add('first story', () => (
+    <div>Hello!</div>
+  ))
---
+++ src/components/repos/repos.story.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+
+storiesOf('Repos', module)
+  .add('first story', () => (
+    <div>Hi!</div>
+  ))
